5aa5a8353e59905d4880572c6efec31641ee371f
Angel Cham <angelchamkailing@gmail.com>
2024-05-27 18:05:51 +0800

    Added Remove, comment, authentication

--- blog/forms.py
+++ blog/forms.py
@@ -1,9 +1,24 @@
 from django import forms
 
-from .models import Post
+from .models import Post, Comment
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 class PostForm(forms.ModelForm):
 
     class Meta:
         model = Post
         fields = ('title', 'text',)
+
+class CommentForm(forms.ModelForm):
+
+    class Meta:
+        model = Comment
+        fields = ('text',)
+
+
+class SignUpForm(UserCreationForm):
+    email = forms.EmailField()
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
